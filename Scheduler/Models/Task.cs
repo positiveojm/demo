c9d60f7275bf2a0aba39e004c7b94d681ef09d31
Ratcliff <oratcliff@syrusinfo.com>
2017-10-14 09:37:07 -0400

    init : auto-generated by .NET

---
+++ Scheduler/Models/Task.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+
+namespace Scheduler.Models
+{
+    public class Task
+    {
+        [Key]
+        public int TaskId { get; set; }
+        public string Description { get; set; }
+        public string Title { get; set; }
+        public DateTime DueDate { get; set; }
+        public bool Status { get; set; }
+        public int UserId { get; set; }
+    }
+}
---
+++ Scheduler/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+
+namespace Scheduler.Models
+{
+    public class User
+    {   [Key]
+        public int UserId { get; set; }
+        public string Name { get; set; }
+    }
+   
+}
